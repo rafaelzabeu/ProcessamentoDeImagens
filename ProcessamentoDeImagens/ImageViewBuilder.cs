8971aa1088cd9fa81b4a533afd4cebacf30413aa
rafaelzabeu <rafaelzabeu@gmail.com>
2017-03-17 18:46:16 -0300

    Initial commit

---
+++ ProcessamentoDeImagens/ImageViewBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessamentoDeImagens
+{
+    public static class ImageViewBuilder
+    {
+        public static Form BuildImageView(string formTitle, Bitmap image)
+        {
+            Form form = new Form();
+            form.Text = formTitle;
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Dock = DockStyle.Fill;
+            pictureBox.Image = image;
+            pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            form.Controls.Add(pictureBox);
+
+            form.ShowDialog();
+
+            return form;
+        }
+    }
+}

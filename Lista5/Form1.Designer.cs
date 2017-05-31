2ed2e20739ad55ff956f1364da5092dff576e6f4
rafaelzabeu <rafaelzabeu@gmail.com>
2017-05-31 20:48:30 -0300

    Lista 5 UI

--- Lista5/Form1.Designer.cs
+++ Lista5/Form1.Designer.cs
@@ -28,12 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.boxColor = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.boxFilter = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.boxSize = new System.Windows.Forms.ComboBox();
+            this.btn_apply = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // boxColor
+            // 
+            this.boxColor.FormattingEnabled = true;
+            this.boxColor.Items.AddRange(new object[] {
+            "Colorida",
+            "Preto e Branco"});
+            this.boxColor.Location = new System.Drawing.Point(12, 24);
+            this.boxColor.Name = "boxColor";
+            this.boxColor.Size = new System.Drawing.Size(121, 21);
+            this.boxColor.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Colorida";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Filtro";
+            // 
+            // boxFilter
+            // 
+            this.boxFilter.FormattingEnabled = true;
+            this.boxFilter.Items.AddRange(new object[] {
+            "Media",
+            "Mediana",
+            "Moda",
+            "Linha Vertical",
+            "Linha Horizontal",
+            "Linha Diagonal Principal",
+            "Linha Diagonal Secundaria",
+            "Borda Normal",
+            "Sobel"});
+            this.boxFilter.Location = new System.Drawing.Point(12, 64);
+            this.boxFilter.Name = "boxFilter";
+            this.boxFilter.Size = new System.Drawing.Size(121, 21);
+            this.boxFilter.TabIndex = 3;
+            this.boxFilter.SelectedIndexChanged += new System.EventHandler(this.boxFilter_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tamanho";
+            // 
+            // boxSize
+            // 
+            this.boxSize.FormattingEnabled = true;
+            this.boxSize.Items.AddRange(new object[] {
+            "3x3",
+            "5x5",
+            "7x7",
+            "9x9"});
+            this.boxSize.Location = new System.Drawing.Point(12, 104);
+            this.boxSize.Name = "boxSize";
+            this.boxSize.Size = new System.Drawing.Size(121, 21);
+            this.boxSize.TabIndex = 5;
+            // 
+            // btn_apply
+            // 
+            this.btn_apply.Location = new System.Drawing.Point(12, 132);
+            this.btn_apply.Name = "btn_apply";
+            this.btn_apply.Size = new System.Drawing.Size(121, 23);
+            this.btn_apply.TabIndex = 6;
+            this.btn_apply.Text = "Aplicar Filtro";
+            this.btn_apply.UseVisualStyleBackColor = true;
+            this.btn_apply.Click += new System.EventHandler(this.btn_apply_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(167, 172);
+            this.Controls.Add(this.btn_apply);
+            this.Controls.Add(this.boxSize);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.boxFilter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.boxColor);
+            this.Name = "Form1";
+            this.Text = "Filtros";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox boxColor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox boxFilter;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox boxSize;
+        private System.Windows.Forms.Button btn_apply;
     }
 }
 
--- Lista5/Form1.cs
+++ Lista5/Form1.cs
@@ -18,18 +18,6 @@ namespace Lista5
         public Form1()
         {
             InitializeComponent();
-
-            Bitmap b = ImageGetter.GetImageFromUser();
-            if (b == null)
-                return;
-
-            Bitmap b2 = FilterUtil.Filter(b, new FilterBorderDetection(3, FilterBorderDetection.FilterType.Sobel));
-            Bitmap b3 = FilterUtil.Filter(b2, new FilterModa(3));
-
-            CreateView("A", b);
-            CreateView("B", b2);
-            CreateView("C", b3);
-
         }
 
         private List<Form> m_openForms = new List<Form>();
@@ -47,5 +35,115 @@ namespace Lista5
         {
             m_openForms.Add(ViewBuilder.BuildImageView(name, image));
         }
+
+        private void boxFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var op = (string)boxFilter.Items[boxFilter.SelectedIndex];
+
+            if (op == "Sobel")
+            {
+                boxSize.Items.Clear();
+                boxSize.Items.Add("3x3");
+                boxSize.Items.Add("5x5");
+
+            }
+            else
+            {
+                boxSize.Items.Clear();
+                boxSize.Items.Add("3x3");
+                boxSize.Items.Add("5x5");
+                boxSize.Items.Add("7x7");
+                boxSize.Items.Add("9x9");
+            }
+
+            boxFilter.SelectedIndex = 0;
+        }
+
+        private bool IsGrayScale()
+        {
+            string op = (string)boxColor.Items[boxColor.SelectedIndex];
+            switch (op)
+            {
+
+                default:
+                case "Colorida":
+                    return false;
+                case "Preto e Branco":
+                    return true;
+            }
+        }
+
+        private int GetScale()
+        {
+            var op = (string)boxSize.Items[boxSize.SelectedIndex];
+            switch (op)
+            {
+                case "3x3":
+                default:
+                    return 3;
+                case "5x5":
+                    return 5;
+                case "7x7":
+                    return 7;
+                case "9x9":
+                    return 9;
+            }
+        }
+
+        private IFilter GetFilter(int size)
+        {
+            IFilter filterToUse;
+            var op = (string)boxFilter.Items[boxFilter.SelectedIndex];
+            switch (op)
+            {
+                default:
+                case "Media":
+                    filterToUse = new FilterMedia(size);
+                    break;
+                case "Mediana":
+                    filterToUse = new FilterMediana(size);
+                    break;
+                case "Moda":
+                    filterToUse = new FilterModa(size);
+                    break;
+                case "Linha Vertical":
+                    filterToUse = new FilterLineDetection(size, FilterLineDetection.LineType.Vertical);
+                    break;
+                case "Linha Horizontal":
+                    filterToUse = new FilterLineDetection(size, FilterLineDetection.LineType.Horizontal);
+                    break;
+                case "Linha Diagonal Principal":
+                    filterToUse = new FilterLineDetection(size, FilterLineDetection.LineType.MainDiagonal);
+                    break;
+                case "Linha Diagonal Secundaria":
+                    filterToUse = new FilterLineDetection(size, FilterLineDetection.LineType.SecondaryDiagonal);
+                    break;
+                case "Borda Normal":
+                    filterToUse = new FilterBorderDetection(size, FilterBorderDetection.FilterType.Normal);
+                    break;
+                case "Sobel":
+                    filterToUse = new FilterBorderDetection(size, FilterBorderDetection.FilterType.Sobel);
+                    break;
+            }
+            return filterToUse;
+        }
+
+        private void btn_apply_Click(object sender, EventArgs e)
+        {
+            closeOtherExercises();
+            Bitmap orig = ImageGetter.GetImageFromUser();
+            if (orig == null)
+                return;
+            if (IsGrayScale())
+                orig = orig.ToGrayScale();
+
+            IFilter filterToUse = GetFilter(GetScale());
+
+            Bitmap result = FilterUtil.Filter(orig, filterToUse);
+
+            CreateView("Original", orig);
+            CreateView("Result", result);
+        }
+
     }
 }
